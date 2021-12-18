b258c415e2417f6cb40cd9f49131582ff2e677a8
manu293 <manojsm293@gmail.com>
2021-12-18 19:30:37 +0530

    Added a resuable header component

---
+++ components/header.js
@@ -0,0 +1,30 @@
+// imports
+
+// local imports
+import headerStyle from "../styles/Header.module.css";
+
+function Header(props) {
+
+    let headerPStyle;
+
+    switch(props.hStyle) {
+        case "h1":
+            headerPStyle = headerStyle.headerStyle1;
+            break;
+        case "h2":
+            headerPStyle = headerStyle.headerStyle2;
+            break;
+        case "h3":
+            headerPStyle = headerStyle.headerStyle3;
+            break;
+        default:
+            headerPStyle = headerStyle.headerStyle3;
+            break;
+    }
+
+    return (
+        <p className={headerPStyle}>{props.hText}</p>
+    )
+}
+
+export default Header;
--- pages/index.js
+++ pages/index.js
@@ -2,17 +2,28 @@
 import Head from 'next/head'
 
 // local imports
-import styles from '../styles/Home.module.css'
+import Header from "../components/header";
+import styles from "../styles/Home.module.css";
 
 function Home() {
   return (
     <div className={styles.homeContainer}>
+
       <Head>
         <title>Airtribe Frontend Assignment</title>
         <meta name="description" content="Airtribe Frontend Assignment" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <p>This is the home page</p>
+
+      <main className="mainSection">
+
+        <Header 
+          hText="Task Board"
+          hStyle="h2"
+        />
+      
+      </main>
+      
     </div>
   )
 }
