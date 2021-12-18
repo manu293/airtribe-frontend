625397ffb229c993de4a9357392f9816d0c6afc8
manu293 <manojsm293@gmail.com>
2021-12-18 18:35:30 +0530

    Added head tag

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,17 @@
 // imports
+import Head from 'next/head'
 
 // local imports
 import styles from '../styles/Home.module.css'
 
 function Home() {
   return (
-    <div className='homeContainer'>
+    <div className={styles.homeContainer}>
+      <Head>
+        <title>Airtribe Frontend Assignment</title>
+        <meta name="description" content="Airtribe Frontend Assignment" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <p>This is the home page</p>
     </div>
   )
